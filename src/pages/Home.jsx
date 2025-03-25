e106ecdb3381a10f61e593ccd1b80112348f90fe
nachoguzman79 <ignacioguzmanok@gmail.com>
2025-03-25 21:26:29 +0100

    new mt-4 in h1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,14 +16,14 @@ const Home = () => {
   
           {/* Columna de texto */}
           <div className="  flex flex-col justify-center items-start text-center lg:text-left order-2 lg:order-1">
-            <h1 className="text-7xl lg:text-7xl xl:text-9xl font-extrabold mb-4 tracking-tight text-left">
-              Hola soy Ignacio
+            <h1 className="mt-4 text-7xl lg:text-7xl xl:text-9xl font-extrabold mb-4 tracking-tight text-left">
+              Hi, I`m Ignacio
             </h1>
             <h2 className="text-4xl lg:text-6xl xl:text-7xl font-semibold tracking-tight text-left">
               Web developer and graphic designer
             </h2>
-            <h3 className="text-xl lg:text-4xl font-bold tracking-tight text-left mt-4">
-              Working on projects
+            <h3 className="text-xl lg:text-3xl font-bold tracking-tight text-left mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus volutpat, ipsum euismod vehicula tincidunt.
             </h3>
           </div>
         </div>
